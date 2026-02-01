694acfeb8f1036df31e9be2caea685b8e87cba02
Wiebe Niehof <wiebe@federated.directory>
2026-02-01 18:15:47 +0100

    feat: sync scalar theme with vitepress dark mode and colors

--- docs/.vitepress/theme/ScalarWrapper.vue
+++ docs/.vitepress/theme/ScalarWrapper.vue
@@ -1,16 +1,22 @@
 <script setup>
 import { ApiReference } from '@scalar/api-reference'
+import { useData } from 'vitepress'
+import { computed } from 'vue'
+
+const { isDark } = useData()
+
+const configuration = computed(() => ({
+  spec: { url: '/swagger.json' },
+  theme: 'default',
+  hideModels: false,
+  darkMode: isDark.value,
+  withDefaultFonts: false // We use our own font in custom.css
+}))
 </script>
 
 <template>
   <div class="scalar-api-reference">
-    <ApiReference 
-      :configuration="{ 
-        spec: { url: '/swagger.json' },
-        theme: 'default',
-        hideModels: false
-      }" 
-    />
+    <ApiReference :configuration="configuration" />
   </div>
 </template>
 
